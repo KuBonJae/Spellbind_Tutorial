17e4f206426e6aeb54fbf40d8ee6c838df9a08b4
KuBonJae <qhswo9@gmail.com>
2025-07-16 14:26:06 +0900

    DOTween - 1
    - DOTween을 활용한 object 조작

--- Assets/Scripts/UsingDOTweenScript.cs
+++ Assets/Scripts/UsingDOTweenScript.cs
@@ -1,18 +1,40 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class UsingDOTweenScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    Sequence sequence;
+
+    // Append : 트윈 마지막에 추가
+    // Insert : 일정 시간에 시작
+    // Join : 앞에 추가된 트윈과 동시 시작
+    // Prepend : 맨 처음에 추가
+    
+    void Awake()
     {
-        
+        sequence = DOTween.Sequence();
+
+        DOTween.Init(false, true).SetCapacity(200, 50);
+
+        //Tweener tr = transform.DOScale(2f, 2f)
+        //    .SetEase(Ease.Linear)
+        //    .OnComplete(() => transform.DOMove(new Vector3(3f, 3f, 0f), 3f));
+
+        gameObject.GetComponent<Rigidbody>()?.transform.DOMove(new Vector3(10f, 10f, 10f), 5f)
+            .OnComplete(TriggerCoroutine);
     }
 
-    // Update is called once per frame
-    void Update()
+    void TriggerCoroutine()
     {
-        
+        StartCoroutine(MakeBigger());
+    }
+
+    IEnumerator MakeBigger()
+    {
+        yield return new WaitForSeconds(1f);
+
+        gameObject.GetComponent<Rigidbody>().transform.DOScale(new Vector3(3f, 3f, 3f), 2f);
     }
 }
